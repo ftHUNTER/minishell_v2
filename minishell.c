5617ae928e5474b256746f03b77dc3512fbb6ec4
rchmouk <rchmouk@student.1337.ma>
2023-05-25 00:54:07 +0100

    2023-05-25 / 12:54

--- minishell.c
+++ minishell.c
@@ -19,9 +19,6 @@ int	main(int ac, char **av, char **env)
 	char	**container;
 	int		len;
 	t_env	*env_list;
-	t_parc	*tmp;
-	int		i;
-	int		j;
 
 	env_list = ft_fill_lst2(env);
 	while (ac && av)
@@ -35,21 +32,6 @@ int	main(int ac, char **av, char **env)
 			container = p_split_pro(buffer, len);
 			all = ft_fill_lst(container);
 			remoev_cout(all);
-			i = 0;
-			tmp = all;
-			j = 0;
-			while (tmp)
-			{
-				i = 0;
-				j = 0;
-				while (tmp->arg[i])
-					printf("%s\n", tmp->arg[i++]);
-				printf("-------\n");
-				while (tmp->name[j])
-					printf("%s\n", tmp->name[j++]);
-				printf("==============================\n");
-				tmp = tmp->next;
-			}
 			free_all(all, container, buffer);
 		}
 		if (buffer == NULL)
